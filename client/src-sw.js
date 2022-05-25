90891e1c60c428b9b6c41ab57132d6911efbf23b
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-05-25 08:08:08 -0500

    starter code for implementing caching of assets

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,41 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+
+// this is a pseudo CacheFirst initializaiton (copied from 20)
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+  // plugins: [
+  //   new CacheableResponsePlugin({
+  //     statuses: [0, 200],
+  //   }),
+  //   new ExpirationPlugin({
+  //     maxAgeSeconds: 30 * 24 * 60 * 60,
+  //   }),
+  // ],
+});
+
 registerRoute();
+
+// registerRoute(({ request }) => request.destination === 'image', assetCache)
+
+// pseudo below
+
+
+// // Register route for caching images
+// // The cache first strategy is often the best choice for images because it saves bandwidth and improves performance.
+// registerRoute(
+//   ({ request }) => request.destination === 'image',
+//   new CacheFirst({
+//     cacheName: 'my-image-cache',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//       new ExpirationPlugin({
+//         maxEntries: 60,
+//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//       }),
+//     ],
+//   })
+// );
